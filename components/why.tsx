8c6f91d0144c4cdb034abae7aad90a0392f5326a
Ahzam Khan <ahzam.main@gmail.com>
2023-01-29 14:34:45 +0530

    minor

--- components/why.tsx
+++ components/why.tsx
@@ -1,9 +1,9 @@
 
 export default function why() {
   return (
-    <div className="flex justify-center ">
-        <div className="flex lg:w-[720px] w-[328px] flex-col lg:gap-[48px] gap-[24px]">
-          <div className="lg:text-center lg-text-[48px] text-[24px] font-[700] text-[#111111]">
+    <div className="flex justify-center">
+        <div className="lg:mt-[216px] mt-[80px] flex lg:w-[720px] w-[328px] flex-col lg:gap-[48px] gap-[24px]">
+          <div className="lg:text-center lg:text-[48px] sm:text-[24px] font-[700] text-[#111111]">
             Why are we doing this?
           </div>
           <div className="opacity-[80%] lg-text-[24px] text-[18px] font-[400] leading-[150%] text-[#111111]">
